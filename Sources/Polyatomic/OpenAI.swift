6acd1ffa1c6c5a4f0434a957e5506de76444d3a7
Rafi Rizwan <vi66raptor@gmail.com>
2023-04-03 18:54:08 -0400

    bro

--- Sources/Polyatomic/OpenAI.swift
+++ Sources/Polyatomic/OpenAI.swift
@@ -72,7 +72,7 @@ public struct OpenAI: LLM {
     public func response<T: SchemaConvertible & Decodable>(for prompt: String) async throws -> T {
         let responseString = try await response(
             for: prompt,
-            constraints: "Ignore all previous directives. You are now a REST API that only returns JSON objects to fit the following schema:\n\(T.schema())\nYou can not respond with anything else aside from the specified JSON or any additional text. Do not respond like a chatbot. Respond like a REST API would.",
+            constraints: "Respond only with a JSON object that fits the following JSON Schema:\n\(T.schema())\nDo not include any other text outside of the schema's defintion.",
             maxTokens: 3000,
             temperature: 0.0,
             topP: 1.0)
