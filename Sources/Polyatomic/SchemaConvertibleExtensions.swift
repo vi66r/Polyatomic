fcb2b2a08783253868a654139e3cdc803bbab462
Rafi Rizwan <vi66raptor@gmail.com>
2023-04-05 03:13:18 -0400

    roll back an earlier change related to reflection which produces unwanted results

--- Sources/Polyatomic/SchemaConvertibleExtensions.swift
+++ Sources/Polyatomic/SchemaConvertibleExtensions.swift
@@ -3,7 +3,3 @@ import Foundation
 extension String: SchemaConvertible {
     public static func schemaConversionInstance() -> String { "" }
 }
-
-extension Array: SchemaConvertible {
-    public static func schemaConversionInstance() -> Array<Element> {[]}
-}
