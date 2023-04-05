7d8af5782718876f0cf07a3128fa5fb84cda3863
Rafi Rizwan <vi66raptor@gmail.com>
2023-04-05 02:49:34 -0400

    allow user to specify an LLM in the then clause

--- Sources/Polyatomic/PolyatomicResponse.swift
+++ Sources/Polyatomic/PolyatomicResponse.swift
@@ -9,18 +9,19 @@ public struct PolyatomicResult<T: Codable & SchemaConvertible> {
         self.result = result
     }
     
-    public func then(_ prompt: String, with parameters: [String: Any]? = nil) async throws -> PolyatomicResult<String> {
+    public func then(_ prompt: String, using model: LLM? = nil, with parameters: [String: Any]? = nil) async throws -> PolyatomicResult<String> {
         let stringRepresentation = try result.stringRepresentation()
         let prompt = "given this data: \"\(stringRepresentation)\"\nperform the following: \"\(prompt)\"\nThere is no need to mention the data used. Respond in a friendly and charming manner" // mood/response type could be a variable or "parameter"
-        return try await llm.response(for: prompt, parameters: parameters)
+        return try await (model ?? llm).response(for: prompt, parameters: parameters)
     }
     
     public func then<M: Decodable & SchemaConvertible>(_ prompt: String,
+                                                       using model: LLM? = nil,
                                                 returningResultContaining type: M.Type,
                                                 with parameters: [String: Any]? = nil
     ) async throws -> PolyatomicResult<M> {
         let stringRepresentation = try result.stringRepresentation()
         let prompt = "given this data: \"\(stringRepresentation)\"\nperform the following: \(prompt)"
-        return try await llm.respond(withResultContaining: type, for: prompt, parameters: parameters)
+        return try await (model ?? llm).respond(withResultContaining: type, for: prompt, parameters: parameters)
     }
 }
